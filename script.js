a732618ccffc50c6b5afc2bc512bf4476aeab0ce
djenki13 <danieledmundjenkins@gmail.com>
2023-11-03 20:28:11 -0400

    parts 1 through 6

--- script.js
+++ script.js
@@ -17,32 +17,40 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+const mainTitle = document.querySelector("#main-title");
+mainTitle.textContent = "DOM Toretto";
 
   // Part 2
-
+const body = document.querySelector("body");
+body.style.backgroundColor("green");
 
   // Part 3
-
+const favThingsList = document.querySelector("#favorite-things");
+const favThingsLast = favThingsList.lastElementChild;
+console.log(favThingsLast);
+favThingsList.removeChild(favThingsLast);
 
   // Part 4
-
+const specialTitles = document.querySelectorAll(".special-title");
+specialTitles.forEach((element) => {
+  element.style.fontSize = "2rem"
+});
 
   // Part 5
-
+const pastRaces = document.querySelector("#past-races");
+pastRaces.removeChild(pastRaces.children[3]);
 
   // Part 6
-
+const newLi = document.createElement("li");
+newLi.innerHTML = "Austin";
+pastRaces.append(newLi);
 
   // Part 7
+
 
 
   // Part 8
 
 
   // Part 9
-
-
-
-
-});
+})
